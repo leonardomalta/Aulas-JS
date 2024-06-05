16accea2d1ed809cf842e957cfa98030c54ce44e
leonardomalta <eng.leonardo.malta@gmail.com>
2024-06-05 15:56:31 +0100

    mains

---
+++ aulas-arrays.js
@@ -0,0 +1,21 @@
+
+
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4];
+const media = soma / notas.length;
+
+console.log('Quantidade de provas = ' + notas.length);
+
+console.log('A média do aluno é ' + media); /* sem estrutura de repetição*/
+
+
+/* com estrutura de repetição*/
+
